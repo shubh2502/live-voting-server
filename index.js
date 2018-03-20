2b74d17f1f566d174ca00672b96b9a905e20d5b2
Shubham Nigam <shubham.nigam@nagarro.com>
2018-03-20 12:37:57 +0530

    Code added for voting server

---
+++ index.js
@@ -0,0 +1,95 @@
+var app = require('http').createServer(handler)
+var io = require('socket.io')(app);
+var fs = require('fs');
+
+app.listen(4300, function () {
+    console.log('Server listening to port: 4300');
+});
+
+function handler(req, res) {
+    fs.readFile(__dirname + '/index.html',
+        function (err, data) {
+            if (err) {
+                res.writeHead(500);
+                return res.end('Error loading index.html');
+            }
+
+            res.writeHead(200);
+            res.end(data);
+        });
+}
+
+var currentRoom = null, dataObj = {};
+
+io.on('connection', function (socket) {
+
+    socket.on('join server room', function (data, fs) {
+        // console.log('Server joined');
+        currentRoom = data.room;
+        socket.join(data.room);
+        socket.broadcast.emit('Join this room', { room: data.room });
+        fs({ room: currentRoom, data: dataObj[currentRoom] });
+    });
+
+    socket.on('Reset data', function (data, fs) {
+        currentRoom = null;
+        dataObj = {};
+        socket.broadcast.emit('Leave this room', { room: currentRoom });
+        fs(currentRoom);
+    });
+
+    socket.on('join room', function (data) {
+        // console.log('çurrent room', currentRoom);
+        socket.join(data.room || currentRoom);
+    });
+
+    socket.on('vote', function (data, fs) {
+        socket.to(data.room || currentRoom).emit('vote_option', data);
+        applyObject(currentRoom, data);
+        //send response
+        fs(currentRoom);
+    });
+
+    socket.on('leave room', function (data) {
+        socket.leave(currentRoom);
+    });
+
+    socket.on('check room', function (room, fs) {
+        var flag = room == currentRoom;
+        fs(flag);
+    });
+
+    socket.on('getTotal', function (data, fs) {
+        var total = getNoOfUsers();
+        fs(total);
+    });
+
+});
+
+function applyObject(currenRoom, data) {
+    dataObj[currenRoom] = dataObj[currenRoom] || {};
+    dataObj[currenRoom].comments = dataObj[currenRoom].comments || '';
+    data.comment = data.comment || '';
+    dataObj[currenRoom].cnt1 = dataObj[currenRoom].cnt1 || 0;
+    dataObj[currenRoom].cnt2 = dataObj[currenRoom].cnt2 || 0;
+    if (data.type === 'option1') {
+        dataObj[currenRoom].cnt1++;
+    } else if (data.type === 'option2') {
+        dataObj[currenRoom].cnt2++;
+    }
+    if (data.comment.length > 2) {
+        dataObj[currenRoom].comments = dataObj[currenRoom].comments + '\n---> ' + data.comment;
+    }
+};
+
+function getNoOfUsers() {
+    // console.log('----------------------------------------------');
+    var clients = io.sockets.adapter.rooms;
+    var total = 0;
+    for (var clientId in clients) {
+        if (io.sockets.connected[clientId])
+            total++;
+    }
+    // console.log('----------------------------------------------');
+    return total;
+}
